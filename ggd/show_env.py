df11b3dab1212003a1bbf07429512c1aa12101de
jbelyeu <jrbelyeu@gmail.com>
2016-10-10 16:36:53 -0600

    checking now to ensure that env vars are updated as well as present

--- ggd/show_env.py
+++ ggd/show_env.py
@@ -12,7 +12,7 @@ def show_env(parser, args):
     pattern = args.pattern if args.pattern else "*"
     conda_env,conda_path = get_conda_env()
     env_filename = os.path.join(conda_path, "etc", "conda","activate.d","env_vars.sh")
-    env_vars = set()
+    env_vars = {}
     try:
         with open(env_filename, "r") as env_file:
             for var in env_file:
@@ -21,18 +21,20 @@ def show_env(parser, args):
                 if len(var_array) >= 2:
                     var_item_array = var_array[1].split("=")
                     if len(var_item_array) >= 1:
-                        env_vars.add(var_item_array[0])
+                        env_vars[var_item_array[0]] = var_item_array[1]
         active_vars,inactive_vars = test_vars(env_vars)
+        print ("*****************************\n")
         if len(active_vars) > 0:
             print ("Active environment variables:")
             for active_var in active_vars:
                 print ("> $" + active_var)
             print()
         if len(inactive_vars) > 0:
-            print ("Inactive environment variables:")
+            print ("Inactive or out-of-date environment variables:")
             for inactive_var in inactive_vars:
                 print ("> $" + inactive_var)
-            print ("\nTo activate inactive vars, run:\nsource activate %s\n" % conda_env)
+            print ("\nTo activate inactive or out-of-date vars, run:\nsource activate %s\n" % conda_env)
+        print ("*****************************\n")
     except IOError:
         print ("No recipe variables found for this environment")
 
@@ -40,7 +42,7 @@ def test_vars(env_vars):
     active_vars = []
     inactive_vars = []
     for env_var in env_vars:
-        if env_var in os.environ and os.environ[env_var] != "":
+        if env_var in os.environ and os.environ[env_var] == env_vars[env_var]:
             active_vars.append(env_var)
         else:
             inactive_vars.append(env_var)
