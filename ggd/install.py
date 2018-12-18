f14d19fda7ab04e4c522da1c38b2349627248508
Michael Cormier <u1138933@kingspeak24.wasatch.peaks>
2018-12-18 10:39:41 -0700

    Update ggd install script

--- ggd/install.py
+++ ggd/install.py
@@ -7,18 +7,21 @@ import os
 import subprocess as sp
 import glob
 from .check_recipe import conda_root
+from .utils import get_species
 from .utils import get_ggd_channels
 from .utils import get_channel_data
-from .search import load_json, search_packages
+from .utils import get_channeldata_url
+from .search import load_json, load_json_from_url, search_packages
+from .uninstall import remove_from_condaroot, check_for_installation
 
-
+SPECIES_LIST = get_species()
 #-------------------------------------------------------------------------------------------------------------
 ## Argument Parser
 #-------------------------------------------------------------------------------------------------------------
 def add_install(p):
 	c = p.add_parser('install', help="install a data recipe from ggd")
-	c.add_argument("name", help="the name of the recipe to install")
 	c.add_argument("-c", "--channel", default="genomics", choices=get_ggd_channels(), help="The ggd channel the desired recipe is stored in. (Default = genomics)")
+	c.add_argument("name", help="the name of the recipe to install")
 	c.set_defaults(func=install)
 
 #-------------------------------------------------------------------------------------------------------------
@@ -32,14 +35,14 @@ def add_install(p):
 #  search the ggd-channel json file. If the recipe exists within the json file,
 #  the installation proceeds. If not, the instalation stops
 def check_ggd_recipe(ggd_recipe,ggd_channel):
-	CHANNEL_DATA = get_channel_data(ggd_channel)
-	jdict = load_json(CHANNEL_DATA)
+	CHANNEL_DATA_URL = get_channeldata_url(ggd_channel)
+	jdict = load_json_from_url(CHANNEL_DATA_URL)
 	package_list = [x[0] for x in search_packages(jdict, ggd_recipe)]
 	if ggd_recipe in package_list:
-		print("\n\t-> %s exists in ggd-%s" %(ggd_recipe,ggd_channel))
+		print("\n\t-> %s exists in the ggd-%s channel" %(ggd_recipe,ggd_channel))
 		return(jdict)
 	else:
-		print("\n\t-> '%s' was not found in ggd." %ggd_recipe)
+		print("\n\t-> '%s' was not found in ggd-%s" %(ggd_recipe, ggd_channel))
 		print("\t-> You can search for recipes using the ggd search tool: \n\t\t'ggd search -t %s'\n" %ggd_recipe)
 		sys.exit()
 
@@ -81,26 +84,25 @@ def check_conda_installation(ggd_recipe):
 # check_S$_bucket
 # ==============
 # Method to check if the recipe is stored on the ggd S3 bucket. If so it installs from S3
-# TODO: 
-# Currently not implemented
-def check_S3_bucket(ggd_recipe):
-	print("S3 NOT implemented yet")
-	return(False)
-	## IF exists, install from here
+def check_S3_bucket(ggd_recipe, ggd_jdict):
+	if "tags" in ggd_jdict["packages"][ggd_recipe]:
+		if "cached" in ggd_jdict["packages"][ggd_recipe]["tags"]:
+			if "uploaded_to_aws" in ggd_jdict["packages"][ggd_recipe]["tags"]["cached"]:
+			print("\n\t-> The %s package is uploaded to an aws S3 bucket. To reduce processing time the package will be downloaded from this aws S3 bucket")
 
 
 # conda_install
 # ============
 # Method to install the recipe from the ggd-channel using conda
-# TODO:
-# Create ggd-genomics channel and add recipes. Current instalation DOES NOT 
-#  work because ggd-genomics does not exist
-def conda_install(ggd_recipe, ggd_channel):
+def conda_install(ggd_recipe, ggd_channel,ggd_jdict):
 	print("\n\t-> Installing %s" %ggd_recipe)
 	try:
 		sp.check_call(["conda", "install", "-c", "ggd-"+ggd_channel, "-y", ggd_recipe], stderr=sys.stderr, stdout=sys.stdout)
 	except sp.CalledProcessError as e:
-		sys.stderr.write("ERROR in install %s" %ggd_recipe)
+		sys.stderr.write("\n\t-> ERROR in install %s\n" %ggd_recipe)
+		sys.stederr.write(e)
+		sys.stderr.write("\n\t-> Removing files created by ggd during installation")
+		check_for_installation(ggd_recipe,ggd_jdict) ## .uninstall method to remove extra ggd files
 		sys.exit(e.returncode)
 
 
@@ -116,8 +118,8 @@ def install(parser, args):
 		## Check if conda has it installed on the system 
 		if not check_conda_installation(args.name):
 			## Check S3 bucket
-			if not check_S3_bucket(args.name):
+			if not check_S3_bucket(args.name, ggd_jsonDict):
 				#if not installed from the S3 bucket install using  conda
-				conda_install(args.name, args.channel)
+				conda_install(args.name, args.channel, ggd_jsonDict)
 			print("\n\t-> DONE")
 				
