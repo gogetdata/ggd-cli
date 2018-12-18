ee7d31dcb47ecf165fbd47b5fdaff5f2fac01925
Michael Cormier <u1138933@kingspeak24.wasatch.peaks>
2018-12-18 10:35:42 -0700

    Add list_pkg_info method

--- ggd/__main__.py
+++ ggd/__main__.py
@@ -8,6 +8,7 @@ from . search import add_search
 from . show_env import add_show_env
 from . install import add_install
 from . uninstall import add_uninstall
+from . list_pkg_info import add_pkg_info
 
 def main(args=None):
     if args is None:
@@ -24,6 +25,8 @@ def main(args=None):
     add_check_recipe(sub)
 
     add_list_files(sub)
+    
+    add_pkg_info(sub)
 
     add_search(sub)
     
