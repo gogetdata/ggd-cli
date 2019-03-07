1334afb321bd23282a9d545f308664f3ab356b53
Michael Cormier <cormiermichaelj@gmail.com>
2019-03-07 15:22:55 -0700

    Update and add functionality to the show-env script. Now handels the removal of conda environment variables during uninstall step Helper functions for install steps

--- ggd/show_env.py
+++ ggd/show_env.py
@@ -3,6 +3,7 @@ import sys
 import os
 import re
 from .check_recipe import check_output
+import subprocess as sp
 
 def add_show_env(p):
     c = p.add_parser('show-env', help="show recipe variables available for this conda environment")
@@ -10,6 +11,15 @@ def add_show_env(p):
     c.set_defaults(func=show_env)
 
 def show_env(parser, args): 
+    """Main method to show the current environment variables created by ggd for the specific conda environment
+
+    show_env
+    ========
+    This method is used to show the ggd environment variables created during a ggd package installation 
+     in the current conda environemnt. It will print out the active and inactive environment variables 
+     and indicate how to actiave inactive environment variables.
+    """
+
     pattern = args.pattern if args.pattern else ".*"
     conda_env,conda_path = get_conda_env()
     env_filename = os.path.join(conda_path, "etc", "conda","activate.d","env_vars.sh")
@@ -51,7 +61,75 @@ def show_env(parser, args):
     finally:
         print ("*****************************\n")
 
+
+def remove_env_variable(env_var):
+    """Method to remove an environment variable if the files are removed from the system
+
+    remove_env_variable
+    ===================
+    This method is used to remove an environment variable from the activate.d/env_var.sh and
+    deactivate.d/env_var.sh files during an uninstall.
+
+    Parameters:
+    -----------
+    1) env_var: The environment variable to remove
+    """
+
+    env_var = env_var.replace("-","_")
+    print("\n\t-> Removing the %s environment variable" %env_var)
+    conda_env, conda_path = get_conda_env()
+    active_env_file = os.path.join(conda_path, "etc", "conda", "activate.d", "env_vars.sh")
+    deactive_env_file = os.path.join(conda_path, "etc", "conda", "deactivate.d", "env_vars.sh")
+    var_list = []
+    with open(active_env_file, "r") as active_env:
+        for var in active_env:
+            if env_var not in var:
+                var_list.append(var.strip())
+    with open(active_env_file, "w") as new_active_env:
+        for var in var_list:
+            new_active_env.write(var + "\n")
+    var_list = []
+    with open(deactive_env_file, "r") as deactive_env:
+        for var in deactive_env:
+            if env_var not in var:
+                var_list.append(var.strip())
+    with open(deactive_env_file, "w") as new_deactive_env:
+        for var in var_list:
+            new_deactive_env.write(var + "\n")
+
+
+def activate_enviroment_variables():
+    """Method to activate the current environment variables
+
+    acticate_environment_variables
+    ==============================
+    This method is used to activate the environments in the current active environment 
+    """
+
+    conda_env, conda_path = get_conda_env()
+    active_env_file = os.path.join(conda_path, "etc", "conda", "activate.d", "env_vars.sh")
+    sp.check_output(["activate", conda_env])
+    sp.check_call(['ggd', "show-env"], stderr=sys.stderr, stdout = sys.stdout)
+
+
 def test_vars(env_vars):
+    """Method to identify the active and inactive environment variables for a specific conda environment
+
+    test_vars
+    =========
+    This mehtod is used to get the active and inactive environment variables for a specifc conda environment
+     created by ggd.
+
+    Parameters:
+    -----------
+    1) env_vars: The list of environment variables to check for activity  
+
+    Return:
+    +++++++
+    1) A list of the active environment variables
+    2) A list of the inactive environment variables
+    """
+
     active_vars = []
     inactive_vars = []
     
@@ -62,7 +140,21 @@ def test_vars(env_vars):
             inactive_vars.append(env_var)
     return active_vars,inactive_vars 
 
+
 def get_conda_env():
+    """Method used to get the current conda environment
+
+    get_conda_env
+    =============
+    This method is used to get the current conda environment used to access the 
+     ggd environment variables created for this specific environment. 
+
+    Returns:
+    ++++++++
+    1) The conda environment name
+    2) The path to the conda environent
+    """
+
     env_info = check_output(["conda", "info", "--envs"])
     fields = env_info.split("\n")
     curr_env = ""
