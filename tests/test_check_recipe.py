ac0f6a5281bb151515fbb5604c256697b94f17bc
Michael Cormier <cormiermichaelj@gmail.com>
2020-04-02 11:37:53 -0600

    Update env var test to use only scaffoldings in hg19.genome

--- tests/test_check_recipe.py
+++ tests/test_check_recipe.py
@@ -1683,13 +1683,24 @@ def test_check_recipe_package_env_vars():
             #!/bin/sh
             set -eo pipefail -o nounset
             genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/hg19/hg19.genome
+            wget --quiet $genome
+
+            ## get the file, 
+            ## unzip it, 
+            ## remove any lines that do not have a scaffolding in the hg19.genom file. (If scaffolding in hg19.genome, grep exists with 0)
+            ## add header to the file, and remove the bin column
+            ## sort it based on the genome file
+            ## bgzip it
             wget --quiet -O - http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/gap.txt.gz \
                 | gzip -dc \
+                | awk '{ if (system("grep -Fq " $2 " hg19.genome") == 0)  print $0}' \
                 | awk -v OFS="\t" 'BEGIN {print "#chrom\tstart\tend\tsize\ttype\tstrand"} {print $2,$3,$4,$7,$8,"+"}' \
                 | gsort /dev/stdin $genome \
                 | bgzip -c > gaps.bed.gz
 
             tabix gaps.bed.gz
+
+            rm hg19.genome
 
         post-link.sh: |
             set -eo pipefail -o nounset
